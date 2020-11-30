21d545e60f0e7d66e36b21bcdc973bbf0ca26e9c
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 14:39:00 -0300

    update

--- app/Controllers/Http/Auth/ProfileController.ts
+++ app/Controllers/Http/Auth/ProfileController.ts
@@ -6,10 +6,12 @@ export default class ProfilesController {
     public async index({ params, response, view, auth }) {
         if(auth.user) {
             const user = await User.find(params.id)
-            return view.render('Auth/profile', { 
-                user: user.toJSON(),
-                user_id: params.id 
-            })
+            if(user) {
+                return view.render('Auth/profile', { 
+                    user: user.toJSON(),
+                    user_id: params.id 
+                })
+            } else return response.redirect('/')
         } else {
             response.redirect('/login')
         }
