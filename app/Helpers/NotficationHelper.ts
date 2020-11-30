34878d3bf4514317cf581b1a2d1963ded1bbd2c4
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 12:55:56 -0300

    funções para notificação criada

---
+++ app/Helpers/NotficationHelper.ts
@@ -0,0 +1,41 @@
+export default class NotificationMessage {
+    type: string
+    text: string
+    message: string
+    icon: string
+
+    typeFlash(type: string) {
+        this.type = type
+    }
+
+    textFlash(text: string) {
+        this.text = text
+    }
+
+    messageFlash(message: string) {
+        this.message = message
+    }
+
+    iconFlash(icon: string) {
+        this.icon = icon
+    }
+
+    notificationFlash(type, text, message, icon) {
+        this.type = type
+        this.text = text
+        this.message = message
+        this.icon = icon
+    }
+
+    public status(session, response) {
+        session.flash({
+            notification: {
+                type: this.type,
+                text: this.text,
+                message: this.message,
+                icon: this.icon
+            }
+        })
+        return response.redirect('back')
+    }
+}
