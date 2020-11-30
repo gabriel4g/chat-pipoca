6d00f610d6fec14dbd57aceb4daad1c64fd04918
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 10:45:10 -0300

    criação de usuário adicionada

--- app/Controllers/Http/Auth/RegistersController.ts
+++ app/Controllers/Http/Auth/RegistersController.ts
@@ -1,11 +1,23 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import User from 'App/Models/User'
+
 export default class RegistersController {
     public index({ view }) {
-        view.render('Auth/register')
+        return view.render('Auth/register')
     }
 
-    public async store() {
+    public async store({ request }) {
+        const user = new User();
 
+        const { username, email, password } = request.all()
+
+        user.username = username
+        user.email = email
+        user.password = password
+        user.photo = '/images/profile.png'
+        user.permission = 2
+
+        await user.save()
     }
 }
