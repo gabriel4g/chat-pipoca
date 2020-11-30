2b0c74ca5195ab55a36e737a90e7a2cd822984b5
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 12:55:03 -0300

    dados atualizados

--- app/Controllers/Http/Auth/LoginController.ts
+++ app/Controllers/Http/Auth/LoginController.ts
@@ -1,19 +1,17 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import Notification from 'App/Helpers/NotficationHelper'
+
 export default class LoginController {
     public index({ view }) {
         return view.render('Auth/login')
     }
 
     public async check({ session, response }) {
-        session.flash({
-            notification: {
-                type: 'danger',
-                text: 'white',
-                message: 'E-mail ou senha, estão incorretos!',
-                icon: 'exclamation'
-            }
-        })
-        return response.redirect('back')
+
+        const message = new Notification()
+
+        message.notificationFlash('danger', 'white', 'E-mail ou senha, estão incorretos!', 'exclamation')
+        message.status(session, response)
     }
 }
