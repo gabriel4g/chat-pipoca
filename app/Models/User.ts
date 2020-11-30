54f33a81c097ee4ed537ecfe97bc161cf9a6cb63
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 10:45:23 -0300

    dados do banco atualizados

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -11,10 +11,34 @@ export default class User extends BaseModel {
   public id: number
 
   @column()
+  public username: string
+
+  @column()
   public email: string
 
   @column({ serializeAs: null })
   public password: string
+
+  @column()
+  public photo: string
+
+  @column()
+  public sex: string
+
+  @column()
+  public location: string
+
+  @column()
+  public relationship: string
+
+  @column()
+  public sexual_orientation: string
+  
+  @column()
+  public color: string
+
+  @column()
+  public permission: number
 
   @column()
   public rememberMeToken?: string
