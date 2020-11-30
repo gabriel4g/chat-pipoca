4e862100ef5591debbc0f709fe7c3142b4413212
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 11:12:20 -0300

    verificação de erros adicionada

--- app/Controllers/Http/Auth/RegisterController.ts
+++ app/Controllers/Http/Auth/RegisterController.ts
@@ -1,23 +1,48 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import User from 'App/Models/User'
+import sessionConfig from 'Config/session';
 
 export default class RegistersController {
     public index({ view }) {
         return view.render('Auth/register')
     }
 
-    public async store({ request }) {
+    public async store({ request, session }) {
         const user = new User();
 
-        const { username, email, password } = request.all()
+        const { username, email, password, response } = request.all()
 
-        user.username = username
-        user.email = email
-        user.password = password
-        user.photo = '/images/profile.png'
-        user.permission = 2
+        try {
+            user.username = username
+            user.email = email
+            user.password = password
+            user.photo = '/images/profile.png'
+            user.permission = 2
 
-        await user.save()
+            const confirm = await user.save()
+
+            if(confirm) {
+                session.flash({
+                    notification: {
+                        type: 'success',
+                        text: 'white',
+                        message: 'Conta criada com sucesso',
+                        icon: 'check'
+                    }
+                })
+                return response.redirect('back')
+            }
+        }catch(err) {
+            session.flash({
+                notification: {
+                    type: 'danger',
+                    text: 'white',
+                    message: 'Não foi possivel salvar os dados!',
+                    icon: 'exclamation'
+                }
+            })
+            return response.redirext('back')
+        }
     }
 }
