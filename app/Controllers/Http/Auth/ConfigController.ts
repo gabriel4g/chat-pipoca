c405c240032e42ad6aafb81a5979f6ba33b4587c
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 16:35:11 -0300

    controlar config adicionado

---
+++ app/Controllers/Http/Auth/ConfigController.ts
@@ -0,0 +1,11 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ConfigsController {
+    public async index({ response, view, auth }) {
+        if(auth.user) {
+            view.render('Auth/configs/config')
+        } else {
+            response.redirect('/login')
+        }
+    }
+}
