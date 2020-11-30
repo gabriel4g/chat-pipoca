bc9da4b1f146bbd4cab125ccff5a53f3ea6be5dc
gabriel4g <gabr.lucena@hotmail.com>
2020-11-30 10:45:33 -0300

    migration atualizada

--- database/migrations/1587988332388_users.ts
+++ database/migrations/1587988332388_users.ts
@@ -6,8 +6,16 @@ export default class UsersSchema extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+      table.string('username', 16).notNullable().unique()
+      table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
+      table.string('photo', 180).notNullable()
+      table.string('sex', 1)
+      table.string('location', 122)
+      table.string('relationship', 55)
+      table.string('sexual_orientation', 24)
+      table.string('color', 7)
+      table.integer('permission').notNullable() // 0 ADM, 1 MOD, 2 USER
       table.string('remember_me_token').nullable()
       table.timestamps(true)
     })
