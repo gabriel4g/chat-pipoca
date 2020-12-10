6689e137fbbde25ac76eaa5a5eb57d9420c56870
gabriel4g <gabr.lucena@hotmail.com>
2020-12-10 14:47:31 -0300

    arquivo atualizado

---
+++ app/Helpers/NotificationHelper.ts
@@ -0,0 +1,41 @@
+export default class NotificationHelper {
+    type: string
+    color: string
+    message: string
+    icon: string
+
+    typeFlash(type: string) {
+        this.type = type
+    }
+
+    textFlash(color: string) {
+        this.color = color
+    }
+
+    messageFlash(message: string) {
+        this.message = message
+    }
+
+    iconFlash(icon: string) {
+        this.icon = icon
+    }
+
+    notificationFlash(type, color, message, icon) {
+        this.type = type
+        this.color = color
+        this.message = message
+        this.icon = icon
+    }
+
+    public status(session, response) {
+        session.flash({
+            notification: {
+                type: this.type,
+                text: this.color,
+                message: this.message,
+                icon: this.icon
+            }
+        })
+        return response.redirect('back')
+    }
+}
