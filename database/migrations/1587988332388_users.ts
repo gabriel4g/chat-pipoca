696851c9ed47ce5d65aabeb0921308a0392d3bcd
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 21:22:17 -0300

    editado

--- database/migrations/1587988332388_users.ts
+++ database/migrations/1587988332388_users.ts
@@ -15,7 +15,7 @@ export default class UsersSchema extends BaseSchema {
       table.string('relationship', 55)
       table.string('sexual_orientation', 24)
       table.string('color', 7)
-      table.integer('permission').notNullable() // 0 ADM, 1 MOD, 2 USER
+      table.integer('permission').notNullable() // 0 ADM, 1 MOD, 2 USER, 3 VIP
       table.string('remember_me_token').nullable()
       table.string('created_at', 55)
     })
