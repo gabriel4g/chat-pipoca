fc2e1a9aefe4ecc19056b634d0a5aaff12e87c75
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 21:13:51 -0300

    mensagem do painel adicionada a ao controller

--- app/Controllers/Http/Config/Panel/ModController.ts
+++ app/Controllers/Http/Config/Panel/ModController.ts
@@ -1,4 +1,16 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import TeamWall from 'App/Models/TeamWall'
+
 export default class ModsController {
+  public async index({ response, auth, view }) {
+    const MESSAGE = await TeamWall.find(1)
+    if(auth.user) {
+      return view.render('Auth/configs/panel/moderator', {
+        message: (MESSAGE)? MESSAGE.message:''
+      })
+    } else {
+      response.redirect('/login')
+    }
+  }
 }
