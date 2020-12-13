a0b54cb2cb62d16e94cab68d7adef3b4a290b09a
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 21:13:08 -0300

    mensagem do painel adicionada a ao controller

--- app/Controllers/Http/Auth/HomeController.ts
+++ app/Controllers/Http/Auth/HomeController.ts
@@ -1,10 +1,16 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import TeamWall from 'App/Models/TeamWall'
+
 export default class HomeController {
     public async index({ auth, response, view }) {
+      const MESSAGE = await TeamWall.find(1)
         if(auth.user) {
                 const USER = await auth.authenticate()
-                return view.render('Auth/home', { USER })
+                return view.render('Auth/home', {
+                  USER,
+                  message: (MESSAGE)? MESSAGE.message:''
+                })
         } else {
                 response.redirect('/login')
         }
