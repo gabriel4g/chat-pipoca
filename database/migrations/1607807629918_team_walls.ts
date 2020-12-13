3ff2224ea99aea67b01eec2dca69f4364f0e5aed
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 23:15:17 -0300

    limite das mensagens atualizado

--- database/migrations/1607807629918_team_walls.ts
+++ database/migrations/1607807629918_team_walls.ts
@@ -6,7 +6,7 @@ export default class TeamWalls extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('message').notNullable()
+      table.string('message', 500).notNullable()
     })
   }
 
