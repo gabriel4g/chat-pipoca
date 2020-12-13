90c09b7449dc95d0135725946d75f2155b9d4700
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 21:13:29 -0300

    mensagem do painel adicionada a ao controller

--- app/Controllers/Http/Config/ConfigController.ts
+++ app/Controllers/Http/Config/ConfigController.ts
@@ -1,10 +1,14 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import TeamWall from 'App/Models/TeamWall'
+
 export default class ConfigsController {
     public async index({ response, view, auth }) {
+      const MESSAGE = await TeamWall.find(1)
         if(auth.user) {
             return view.render('Auth/configs/config', {
-              user: auth.user
+              user: auth.user,
+              message: (MESSAGE)? MESSAGE.message:''
             })
         } else {
             response.redirect('/login')
