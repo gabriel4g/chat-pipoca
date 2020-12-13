4c2fc166d8f9889f4ae0c73a3ef474b48308ca4f
gabriel4g <gabr.lucena@hotmail.com>
2020-12-12 21:15:43 -0300

    migration, criação da tabela para armezenar a mensagem do mural da equipe

---
+++ database/migrations/1607807629918_team_walls.ts
@@ -0,0 +1,16 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class TeamWalls extends BaseSchema {
+  protected tableName = 'team_walls'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('message').notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
