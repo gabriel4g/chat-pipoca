ac0cbf894c3b8625b2feb1ce0b4c02a0edc1deac
gabriel4g <gabr.lucena@hotmail.com>
2020-12-21 14:17:52 -0300

    Modelo do chat principal

---
+++ app/Models/Chat.ts
@@ -0,0 +1,21 @@
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import User from 'App/Models/User'
+
+export default class Chat extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+ @column()
+ public user_id: number
+
+ @column()
+ public messages: string
+
+ @column()
+  public userId: number
+
+  @belongsTo(() => User, {
+
+  })
+  public user: BelongsTo<typeof User>
+}
