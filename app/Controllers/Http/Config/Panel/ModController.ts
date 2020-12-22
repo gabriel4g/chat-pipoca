f899cbbae9817e258f2b26c12d0f8fc485231c81
gabriel4g <gabr.lucena@hotmail.com>
2020-12-21 21:53:04 -0300

    sistema de claro/escuro automático

--- app/Controllers/Http/Config/Panel/ModController.ts
+++ app/Controllers/Http/Config/Panel/ModController.ts
@@ -1,13 +1,18 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import TeamWall from 'App/Models/TeamWall'
+import Light from 'App/Locales/style/styleLight'
+import Dark from 'App/Locales/style/styleDark'
+import StyleHelper from 'App/Helpers/StyleHelper'
 
 export default class ModsController {
   public async index({ response, auth, view }) {
     const MESSAGE = await TeamWall.find(1)
     if(auth.user) {
       return view.render('Auth/configs/panel/moderator', {
-        message: (MESSAGE)? MESSAGE.message:''
+        message: (MESSAGE)? MESSAGE.message:'',
+        style: (StyleHelper.styleSecondary() == 'Dark')? Dark:Light,
+        styleDefault: StyleHelper.style()
       })
     } else {
       response.redirect('/login')
