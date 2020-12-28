8c76fa9e939e0bb4ebfc82457b86a0b107373fa3
gabriel4g <gabr.lucena@hotmail.com>
2020-12-28 14:32:19 -0300

    controller, página comandos bbcodes

---
+++ app/Controllers/Http/Config/BbcodesController.ts
@@ -0,0 +1,23 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import TeamWall from 'App/Models/TeamWall'
+import Light from 'App/Locales/style/styleLight'
+import Dark from 'App/Locales/style/styleDark'
+import StyleHelper from 'App/Helpers/StyleHelper'
+
+export default class BbcodesController {
+  public async index({ response, view, auth }) {
+    const MESSAGE = await TeamWall.find(1)
+      if(auth.user) {
+        return view.render('Auth/configs/bbcodes', {
+          user: auth.user,
+          message: (MESSAGE)? MESSAGE.message:'',
+          style: (StyleHelper.styleSecondary() == 'Dark')? Dark:Light,
+          styleDefault: StyleHelper.style()
+        })
+      } else {
+        response.redirect('/login')
+      }
+  }
+
+}
