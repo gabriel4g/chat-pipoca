22b87845ae36eabff2c0af351230e6ee8a659a80
Gabriel Oliveira <gabr.lucena@hotmail.com>
2021-07-15 13:12:28 -0300

    add new route and middleware

--- app/Modules/Login/routes.ts
+++ app/Modules/Login/routes.ts
@@ -1,4 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/signin', 'LoginController.index')
+Route.get('/signin', 'LoginController.index').middleware('logged')
 Route.post('/signin', 'LoginController.show')
+
+Route.get('/logout', 'LoginController.logout')
