9bb80433132759e9420b9ae21c85fb72bdae9212
Gabriel Oliveira <gabr.lucena@hotmail.com>
2021-07-15 13:12:39 -0300

    add middleware('logged')

--- app/Modules/Register/routes.ts
+++ app/Modules/Register/routes.ts
@@ -1,4 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/join', 'RegisterController.index')
+Route.get('/join', 'RegisterController.index').middleware('logged')
 Route.post('/join', 'RegisterController.store')
